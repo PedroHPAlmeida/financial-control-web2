269a72746b35072b802cd12b71992442637f3bd9
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2025-02-08 12:30:08 -0300

    feat: adiciona comportamento dinâmico ao Hello de acordo com as rotas

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  date = new Date();
-
-  @Input() showHello: boolean = false;
+export class HeaderComponent implements OnInit, OnDestroy {
   @Input({ required: true }) name: string = '';
+
+  date = new Date();
+  showHello: boolean = false;
+
+  private routeSubscription!: Subscription;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.routeSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.updateShowHello();
+      });
+
+    this.updateShowHello();
+  }
+
+  ngOnDestroy() {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
+
+  private updateShowHello() {
+    const currentRoute = this.activatedRoute.snapshot.firstChild?.routeConfig?.path;
+    const showHelloRoutes = ['transactions'];
+    this.showHello = currentRoute ? showHelloRoutes.includes(currentRoute) : false;
+  }
+
 }
